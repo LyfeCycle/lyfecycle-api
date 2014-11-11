3518f7e115486156101733100dead27169f50d01
Deven <devend711@gmail.com>
2014-11-10 20:12:30 -0500

    use morgan

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var cool = require('cool-ascii-faces');
 var locations = require('./routes/locations');
@@ -9,7 +10,8 @@ app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-
 app.use(bodyParser.json()); // parse application/json
 
 var portNum = 3000;
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
+app.use(morgan('combined'));
 
 // location routes
 
