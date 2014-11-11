b9bde828d49646bb7c43657de4215fadbb478b85
Deven <devend711@gmail.com>
2014-11-11 00:11:19 -0500

    minor work

--- database.js
+++ database.js
@@ -1,5 +1,4 @@
 var mongo = require('mongodb');
 var monk = require('monk');
-var port = (process.env.MONGOLAB_URI || 'localhost');
-
-var db = monk(port + ':3000/locationsdb');
+var mongoURI = (process.env.MONGOLAB_URI || 'localhost');
+var locationdb = require('monk')(mongoURI + '/locationdb')
