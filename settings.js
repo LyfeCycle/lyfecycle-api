6a3e219cd7e1e3df3d4d73cbfdcaa934b2f75597
Deven <devend711@gmail.com>
2014-11-11 03:49:10 -0500

    try to authenticate mongo

--- settings.js
+++ settings.js
@@ -2,6 +2,6 @@ var secrets = require('./secrets');
 
 module.exports.portNum = process.env.PORT || 3000;
 module.exports.mongoPort = 27017;
-//module.exports.mongoURI = (process.env.MONGOLAB_URI ? ('mongodb://' + secrets.dbUser + ':' + secrets.dbPassword + '@ds051630.mongolab.com:51630') :  ('localhost:' + module.exports.mongoPort));
-module.exports.mongoURI = (process.env.MONGOLAB_URI ||  ('localhost:' + module.exports.mongoPort));
+module.exports.mongoURI = (process.env.MONGOLAB_URI ? ('mongodb://' + secrets.dbUser + ':' + secrets.dbPassword + '@ds051630.mongolab.com:51630') :  ('localhost:' + module.exports.mongoPort));
+//module.exports.mongoURI = (process.env.MONGOLAB_URI || ('localhost:' + module.exports.mongoPort));
 
