71908a06f45ce114696b1df54f4522377100a842
Deven <devend711@gmail.com>
2015-03-22 17:03:11 -0400

    use .env vars

--- secrets-example.js
+++
@@ -1,5 +0,0 @@
-// for localhost testing only - on heroku, these vars are enviornment variables with the same name
-// e.g. use process.env.SECRET
-// save a copy of this as secrets.js in the root folder and fill out with appropriate values
-
-module.exports.GOOGLE_KEY = 'REPLACE_THIS'
