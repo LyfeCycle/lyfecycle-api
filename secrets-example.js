958a58e560302aabc9563a85c1d85b3964322635
Deven <devend711@gmail.com>
2015-03-22 16:51:11 -0400

    use environment var for google key

--- secrets-example.js
+++ secrets-example.js
@@ -1,1 +1,4 @@
-module.exports.googleKey = 'abc'
+// for localhost testing only - on heroku, these vars are enviornment variables with the same name
+// e.g. use process.env.SECRET
+
+module.exports.GOOGLE_KEY = 'abc'
